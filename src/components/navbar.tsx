aa3937e94086364d38baf7be2a33e7a6e32b4bc7
Shreehari Acharya <shreehari.acharya.06@gmail.com>
2025-07-01 13:54:10 +0530

    Add login page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,10 @@
 import { InvertBtn, InvertBtn2 } from "./ui/custom-btns";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
+
+    const router = useRouter();
+
     return (
         <div
             className="top-3 inset-x-0 max-w-4xl mx-auto z-50 border-1 py-2 px-6 rounded-full flex items-center font-light justify-between gap-4
@@ -16,8 +20,7 @@ export default function Navbar() {
                 <a href="/pricing"> Pricing</a>
             </div>
             <div className="flex gap-4">
-                <InvertBtn2 text="Log in" />
-                <InvertBtn text="Sign up" />
+                <InvertBtn2 text="Log in" onClick={() => {router.push("/login")}} />
             </div>
         </div>
     )
--- src/components/ui/custom-btns.tsx
+++ src/components/ui/custom-btns.tsx
@@ -32,10 +32,12 @@ export function GradientBorderBtn({text}: {text: string}) {
     )
 }
 
-export function GradientBorderBtn2({text}: {text: string}) {
+export function GradientBorderBtn2({text, onClick}: {text: string, onClick?: () => void}) {
     return (
         <button className="relative inline-flex h-9 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
-                  md:h-13">
+                  md:h-13"
+                onClick={onClick}  
+                  >
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
         <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 text-sm font-light text-white backdrop-blur-3xl
                 sm:text-base sm:px-5 sm:py-1
@@ -46,21 +48,25 @@ export function GradientBorderBtn2({text}: {text: string}) {
     )
 }
 
-export function InvertBtn({ text }: { text: string }) {
+export function InvertBtn({ text, onClick }: { text: string, onClick?: () => void }) {
     return (
         <button className="px-4 py-1 text-xs rounded-full bg-blue-400 text-white font-light transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-teal-500
                  sm:text-sm
-                 md:text-base">
+                 md:text-base"
+                onClick={onClick} 
+                >
             {text}
         </button>
     )
 }
 
-export function InvertBtn2({ text }: { text: string }) {
+export function InvertBtn2({ text, onClick }: { text: string, onClick?: () => void }) {
     return (
         <button className="shadow-[inset_0_0_0_2px_#616467] text-xs text-black px-4 py-2 rounded-full font-light bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200
                  sm:text-sm
-                 md:text-base">
+                 md:text-base"
+                onClick={onClick} 
+                >
             {text}
         </button>
     )
