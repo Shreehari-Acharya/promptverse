515fa9ef783151e4be44ec948233bba1bb8a76de
Shreehari Acharya <shreehari.acharya.06@gmail.com>
2025-07-02 16:59:16 +0530

    New shadcn components

--- frontend/src/components/ui/custom-btns.tsx
+++ frontend/src/components/ui/custom-btns.tsx
@@ -1,10 +1,12 @@
 
 
-export function GradientBorderBtn({text}: {text: string}) {
+export function GradientBorderBtn({text, onClick}: {text: string, onClick: () => void}) {
     return (
         <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-sm font-light leading-6  text-white inline-block
                  sm:text-base
-                 lg:text-lg">
+                 lg:text-lg"
+                 onClick={onClick}
+                 >
         <span className="absolute inset-0 overflow-hidden rounded-full">
           <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
         </span>
